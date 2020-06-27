fc1513069a6a3ca238ef9b1c7b1e85b468d06895
Luan Azevedo <azeveedolh@gmail.com>
2020-06-27 20:57:21 -0300

    start development of access to github public api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,60 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import logoImg from '../../assets/github-logo.svg';
 import { Title, Form, Repositories } from './styles';
+import api from '../../services/api';
 
-const Dashboard: React.FC = () => (
-  <>
-    <img src={logoImg} alt="Github Explorer" />
-    <Title>Explore repositórios no Github</Title>
+const Dashboard: React.FC = () => {
+  const [repositories, setRepositories] = useState([]);
 
-    <Form>
-      <input placeholder="Digite o nome do repositório." />
-      <button type="submit">Pesquisar</button>
-    </Form>
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
 
-    <Repositories>
-      <a href="repository">
-        <img
-          src="https://avatars0.githubusercontent.com/u/51171908?s=460&u=09f2687b76fc38d1270c930efed483973597fdad&v=4"
-          alt="Luan Azevedo"
-        />
+      <Form>
+        <input placeholder="Digite o nome do repositório." />
+        <button type="submit">Pesquisar</button>
+      </Form>
 
-        <div>
-          <strong>first-react-project</strong>
-          <p>Small project to practice the concepts of react</p>
-        </div>
+      <Repositories>
+        <a href="repository">
+          <img
+            src="https://avatars0.githubusercontent.com/u/51171908?s=460&u=09f2687b76fc38d1270c930efed483973597fdad&v=4"
+            alt="Luan Azevedo"
+          />
 
-        <FiChevronRight size={20} />
-      </a>
-      <a href="repository">
-        <img
-          src="https://avatars0.githubusercontent.com/u/51171908?s=460&u=09f2687b76fc38d1270c930efed483973597fdad&v=4"
-          alt="Luan Azevedo"
-        />
+          <div>
+            <strong>first-react-project</strong>
+            <p>Small project to practice the concepts of react</p>
+          </div>
 
-        <div>
-          <strong>first-react-project</strong>
-          <p>Small project to practice the concepts of react</p>
-        </div>
-
-        <FiChevronRight size={20} />
-      </a>
-      <a href="repository">
-        <img
-          src="https://avatars0.githubusercontent.com/u/51171908?s=460&u=09f2687b76fc38d1270c930efed483973597fdad&v=4"
-          alt="Luan Azevedo"
-        />
-
-        <div>
-          <strong>first-react-project</strong>
-          <p>Small project to practice the concepts of react</p>
-        </div>
-
-        <FiChevronRight size={20} />
-      </a>
-    </Repositories>
-  </>
-);
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
 
 export default Dashboard;
